43f246355ec280928b5cda03e9db59c10dea77ad
Ton Alexandre <ton.vibecoding@gmail.com>
2026-02-19 23:26:49 -0300

    Fix(Vite): Update base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/GamesDavi/', // Absolute base path for GitHub Pages
   resolve: {}
 })
